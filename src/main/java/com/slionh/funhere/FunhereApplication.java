4df121ca25877f4cb804bb5453108abd2b31b473
slionh <841619348@qq.com>
2018-08-22 12:19:56 +0800

    initial

---
+++ src/main/java/com/slionh/funhere/FunhereApplication.java
@@ -0,0 +1,12 @@
+package com.slionh.funhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunhereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunhereApplication.class, args);
+    }
+}
